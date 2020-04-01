eb36d6db72b171ebe442177bd3e94d216554377e
shashank <sss41097@gmail.com>
2020-04-01 22:54:27 +0530

    Initial commit

---
+++ src/FormComponent.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function FormComponent(props) {
+
+console.log(props.data.topText)
+
+    return (
+        <div>
+            <form className='meme-form' onSubmit={ props.handleSubmit }>
+                <input 
+                    type='text'
+                    name='topText'
+                    value={ props.data.topText }
+                    placeholder='Top Text'
+                    onChange={ props.handleChange }
+                />
+                <input 
+                    type='text'
+                    name='bottomText'
+                    value={ props.data.bottomText }
+                    placeholder='Bottom Text'
+                    onChange={ props.handleChange }
+                />
+                <button>Generate!</button>
+            </form>
+        </div>
+    )
+}
+
+export default FormComponent
---
+++ src/MemeGenerator.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+
+import FormComponent from './FormComponent'
+import MemeComponent from './MemeComponent'
+
+class MemeGenerator extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            topText: '',
+            bottomText: '',
+            randomImage: 'https://i.imgflip.com/1bij.jpg',
+            allMemeImages: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount() {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                this.setState({ allMemeImages: memes })
+            })
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const randomIndex = Math.floor(Math.random() * this.state.allMemeImages.length)
+        const randomImage = this.state.allMemeImages[randomIndex].url
+        this.setState({ randomImage: randomImage })
+    }
+
+    render() {
+        return (
+            <div>
+                <FormComponent 
+                    data={ this.state }
+                    handleChange={ this.handleChange }
+                    handleSubmit={ this.handleSubmit }
+                />
+                <MemeComponent data={this.state}/>
+            </div>
+        )
+    }
+}
+
+export default MemeGenerator
